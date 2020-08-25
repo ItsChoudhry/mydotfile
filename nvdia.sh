ee4bcb2c5069fc9cd38f79c029348ad5d546ab87
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2020-08-25 20:22:58 +0100

    Add monitor fixing script

---
+++ nvdia.sh
@@ -0,0 +1,1 @@
+nvidia-settings --assign CurrentMetaMode="HDMI-0: nvidia-auto-select @1920x1080 +1920+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
