024d32c58d2f4dc9d8853b70a594a1562e0919df
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2022-02-03 18:47:25 +0000

    Add setup symlinks file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -sf /home/choudhry/repos/mydotfile/.zshrc /home/choudhry/.zshrc
+ln -sf /home/choudhry/repos/mydotfile/.p10k.zsh /home/choudhry/.p10k.zsh
+ln -sf /home/choudhry/repos/mydotfile/.vimrc /home/choudhry/.vimrc
+ln -sf /home/choudhry/repos/mydotfile/aliases.zsh /home/choudhry/.oh-my-zsh/custom/aliases.zsh
+ln -sf /home/choudhry/repos/mydotfile/.tmux.conf /home/choudhry/.tmux.conf
